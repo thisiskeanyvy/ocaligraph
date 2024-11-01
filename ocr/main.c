e1c199ab5f902e6f835f9a7644b2c69155e0531c
thisiskeanyvy <keanyvy@keemail.me>
2024-11-01 11:34:11 +0100

    ocr menu

--- ocr/main.c
+++ ocr/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+void help() {
+	printf("menu");
+}
+
+int main(int argc, char* argv[]) {
+	for(int i = 1; i < argc; i++) {
+		if(strcmp(argv[i],"--help") == 0) {
+			help();
+		}
+	}
 	return 0;
 }
