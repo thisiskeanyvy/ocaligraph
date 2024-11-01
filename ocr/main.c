e77c448f61750801c456d336af4bc86fdefe66d7
thisiskeanyvy <keanyvy@keemail.me>
2024-11-01 11:58:10 +0100

    ocr menu

--- ocr/main.c
+++ ocr/main.c
@@ -3,13 +3,20 @@
 #include <string.h>
 
 void help() {
-	printf("menu");
+	printf("OCaligraph v1.0 ( https://ocaligraph.com )\n"
+       "Usage: ocaligraph [Scan Type(s)] [Options] {target specification}\n"
+       "TARGET SPECIFICATION:\n"
+       "SEE THE MAN PAGE (https://ocaligraph.com/fr/ocr/man) FOR MORE OPTIONS AND EXAMPLES\n");
 }
 
 int main(int argc, char* argv[]) {
-	for(int i = 1; i < argc; i++) {
-		if(strcmp(argv[i],"--help") == 0) {
-			help();
+	if(argc == 1) {
+		help();
+	} else {
+		for(int i = 1; i < argc; i++) {
+			if(strcmp(argv[i],"--help") == 0) {
+				help();
+			}
 		}
 	}
 	return 0;
